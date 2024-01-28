c8da68dd773f4afe662195e76e5b5b6abfaa2d58
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-01-28 12:26:19 +0100

    Added Prettier config to ESLint

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import styles from './page.module.css';
 
 export default function App() {
-  return <h1 className={styles.title}>To-don't list</h1>;
+  return <h1 className={styles.title}>To-don&apos;t list</h1>;
 }
