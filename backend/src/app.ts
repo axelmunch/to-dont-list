0d97f779d685b163d9c592ca6d407f547739af7e
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-01-29 21:22:59 +0100

    Added CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
 import routes from './routes/index.js';
 
 const app = Fastify({
   logger: true,
 });
+
+// CORS
+await app.register(fastifyCors);
 
 // Routes
 routes.forEach((route) => {
